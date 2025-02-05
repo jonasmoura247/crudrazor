db405c600eba6823b804f53b1c1d56c7c74d1183
jonasmoura247 <jonas.moura.cavalcante@hotmail.com>
2025-02-04 22:35:53 -0300

    Feito CRUD, testando RAZOR.

---
+++ CrudProdutos/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CrudProdutos.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ CrudProdutos/Models/Produto.cs
@@ -0,0 +1,10 @@
+﻿namespace CrudProdutos.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+        public int Quantidade { get; set; }
+    }
+}
