8be55791b3c64fef3abc5652fc1f44430d424043
jonasmoura247 <jonas.moura.cavalcante@hotmail.com>
2025-02-05 08:03:01 -0300

    comentarios em ProdutosController

--- CrudProdutos/Controllers/ProdutosController.cs
+++ CrudProdutos/Controllers/ProdutosController.cs
@@ -14,18 +14,31 @@ namespace CrudProdutos.Controllers
     {
         private readonly AppDbContext _context;
 
+        /// <summary>
+        /// Construtor que recebe o contexto do banco de dados.
+        /// </summary>
+        /// <param name="context">Instância do banco de dados.</param>
+
         public ProdutosController(AppDbContext context)
         {
             _context = context;
         }
 
+        /// <summary>
+        /// Exibe a lista de produtos disponíveis.
+        /// </summary>
+        /// <returns>Uma View com a lista de produtos.</returns>
         // GET: Produtos
         public async Task<IActionResult> Index()
         {
             var produtos = await _context.Produtos.Where(p => p.Quantidade > 0).ToListAsync();
             return View(produtos);
         }
-
+        /// <summary>
+        /// Exibe os detalhes de um produto específico.
+        /// </summary>
+        /// <param name="id">ID do produto.</param>
+        /// <returns>Uma View com os detalhes do produto.</returns>
         // GET: Produtos/Details/5
         public async Task<IActionResult> Details(int? id)
         {
@@ -44,15 +57,20 @@ namespace CrudProdutos.Controllers
             return View(produto);
         }
 
-        // GET: Produtos/Create
+        /// <summary>
+        /// Retorna a View para criar um novo produto.
+        /// </summary>
+        /// <returns>Uma View para criação de um produto.</returns>
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: Produtos/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        /// <summary>
+        /// Processa o envio do formulário para criação de um novo produto.
+        /// </summary>
+        /// <param name="produto">Objeto produto preenchido pelo usuário.</param>
+        /// <returns>Redireciona para a página inicial ou retorna a View caso haja erro.</returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,Nome,Preco,Quantidade")] Produto produto)
@@ -66,7 +84,11 @@ namespace CrudProdutos.Controllers
             return View(produto);
         }
 
-        // GET: Produtos/Edit/5
+        /// <summary>
+        /// Retorna a View para edição de um produto existente.
+        /// </summary>
+        /// <param name="id">ID do produto a ser editado.</param>
+        /// <returns>Uma View para edição do produto.</returns>
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -82,9 +104,12 @@ namespace CrudProdutos.Controllers
             return View(produto);
         }
 
-        // POST: Produtos/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        /// <summary>
+        /// Processa a edição de um produto existente.
+        /// </summary>
+        /// <param name="id">ID do produto.</param>
+        /// <param name="produto">Objeto produto atualizado.</param>
+        /// <returns>Redireciona para a lista de produtos ou retorna a View caso haja erro.</returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,Preco,Quantidade")] Produto produto)
@@ -103,7 +128,7 @@ namespace CrudProdutos.Controllers
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!ProdutoExists(produto.Id))
+                    if (!ProdutoExiste(produto.Id))
                     {
                         return NotFound();
                     }
@@ -117,7 +142,11 @@ namespace CrudProdutos.Controllers
             return View(produto);
         }
 
-        // GET: Produtos/Delete/5
+        /// <summary>
+        /// Retorna a View de confirmação para deletar um produto.
+        /// </summary>
+        /// <param name="id">ID do produto.</param>
+        /// <returns>Uma View para confirmar a exclusão.</returns>
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -135,7 +164,11 @@ namespace CrudProdutos.Controllers
             return View(produto);
         }
 
-        // POST: Produtos/Delete/5
+        /// <summary>
+        /// Processa a exclusão de um produto do banco de dados.
+        /// </summary>
+        /// <param name="id">ID do produto a ser excluído.</param>
+        /// <returns>Redireciona para a página inicial.</returns>
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
@@ -149,8 +182,12 @@ namespace CrudProdutos.Controllers
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
-
-        private bool ProdutoExists(int id)
+        /// <summary>
+        /// Verifica se um produto existe no banco de dados.
+        /// </summary>
+        /// <param name="id">ID do produto.</param>
+        /// <returns>True se o produto existir, False caso contrário.</returns>
+        private bool ProdutoExiste(int id)
         {
             return _context.Produtos.Any(e => e.Id == id);
         }
--- CrudProdutos/obj/Debug/net9.0/CrudProdutos.AssemblyInfo.cs
+++ CrudProdutos/obj/Debug/net9.0/CrudProdutos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CrudProdutos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b2e22875280bfb62fbaf07d9f135d32540a029c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db405c600eba6823b804f53b1c1d56c7c74d1183")]
 [assembly: System.Reflection.AssemblyProductAttribute("CrudProdutos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CrudProdutos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
